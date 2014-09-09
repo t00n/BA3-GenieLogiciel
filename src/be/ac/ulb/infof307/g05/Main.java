88ad8ff55cf5a9585f38dd7190b10208ea2bda73
Jean-Sébastien Lerat <Jean-Sebastien.Lerat@ulb.ac.be>
2014-09-09 10:53:05 +0200

    Initial import

---
+++ src/be/ac/ulb/infof307/g05/Main.java
@@ -0,0 +1,13 @@
+package be.ac.ulb.infof307.g05;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/be/ac/ulb/infof307/g05/TestMain.java
@@ -0,0 +1,14 @@
+package be.ac.ulb.infof307.g05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
