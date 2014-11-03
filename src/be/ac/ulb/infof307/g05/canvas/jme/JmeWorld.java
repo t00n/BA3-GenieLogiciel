85c71ee8975a2f9fb6fa10d3c5a0f49644fef3d5
macornil <martin.cornil@ulb.ac.be>
2014-11-03 16:36:05 +0100

    [ITER1] delete teapot test

--- src/be/ac/ulb/infof307/g05/canvas/jme/JmeWorld.java
+++ src/be/ac/ulb/infof307/g05/canvas/jme/JmeWorld.java
@@ -2,61 +2,65 @@ package be.ac.ulb.infof307.g05.canvas.jme;
 
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
 
 public class JmeWorld extends SimpleApplication {
 
+	private Camera[] _camera = new Camera[2];
+	private ViewPort[] _view = new ViewPort[2];
 	
 	public JmeWorld(){
 	}
 
-	
+	private void createViews(){
+		_camera[0] = cam;
+        _camera[0].setViewPort(0.0f, 1.0f, 0f, 0.5f);
+        _camera[0].setLocation(new Vector3f(5f, 5f, 5f));
+        _camera[0].lookAt(new Vector3f(0f, 0f, 0f), new Vector3f(0f, 1f, 0f));
+        _view[0] = viewPort;
+        _view[0].setBackgroundColor(ColorRGBA.White);
+        
+        _camera[1] = _camera[0].clone();
+        _camera[1].setViewPort(0.0f, 1.0f, 0.5f, 1.0f);
+        _camera[1].setLocation(new Vector3f(0.0f, 0.0f, 5.0f));
+        _camera[1].lookAt(new Vector3f(0f, 0f, 0f), new Vector3f(0f, 1f, 0f));
+        _view[1] = renderManager.createMainView("Top", _camera[1]);
+
+	}
+
 	public void simpleInitApp(){
-        // create the geometry and attach it
-        Geometry teaGeom = (Geometry) assetManager.loadModel("Models/Teapot/Teapot.obj");
-        teaGeom.scale(3);
+	   guiViewPort.setEnabled(false);
+	   createViews();
+	   
+	   test();
+	}
+	
+	private void disable2d(){
+		_camera[0].setViewPortTop(1);
+		_camera[1].setViewPortBottom(0);
+		_view[1].setEnabled(false);		
+	}
+	
+	void test(){
+		Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+        Material mat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Red);
+        geom.setMaterial(mat);
+        rootNode.attachChild(geom);
 
-        DirectionalLight dl = new DirectionalLight();
-        dl.setColor(ColorRGBA.White);
-        dl.setDirection(Vector3f.UNIT_XYZ.negate());
-
-        rootNode.addLight(dl);
-        rootNode.attachChild(teaGeom);
-
-        // Setup first view
-        viewPort.setBackgroundColor(ColorRGBA.Blue);
-        cam.setViewPort(0.0f, 1.0f, 0f, 0.5f);
-        cam.setLocation(new Vector3f(3.3212643f, 4.484704f, 4.2812433f));
-        cam.setRotation(new Quaternion(-0.07680723f, 0.92299235f, -0.2564353f, -0.27645364f));
         
         flyCam.setDragToRotate(true);
-        
-        // Setup second view
-        Camera cam2 = cam.clone();
-        cam2.setViewPort(0.0f, 1.0f, 0.5f, 1.0f);
-        cam2.setLocation(new Vector3f(-0.10947256f, 1.5760219f, 4.81758f));
-        cam2.setRotation(new Quaternion(0.0010108891f, 0.99857414f, -0.04928594f, 0.020481428f));
 
-        ViewPort view2 = renderManager.createMainView("Bottom Left", cam2);
-        view2.setClearFlags(true, true, true);
-        view2.attachScene(rootNode);
-
-
-        //test multiview for gui
-        guiViewPort.getCamera().setViewPort(.5f, 1f, .5f, 1f);
-
-        // Setup second gui view
-        Camera guiCam2 = guiViewPort.getCamera().clone();
-        guiCam2.setViewPort(0f, 0.5f, 0f, 0.5f);
-        ViewPort guiViewPort2 = renderManager.createPostView("Gui 2", guiCam2);
-        guiViewPort2.setClearFlags(false, false, false);
-        guiViewPort2.attachScene(guiViewPort.getScenes().get(0));
+        _view[0].attachScene(rootNode);
+        _view[1].attachScene(rootNode);
+        disable2d();
 	}
 }
