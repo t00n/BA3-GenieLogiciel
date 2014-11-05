813f884aaafcb3a3d0f9e74578e9e073b64e27f2
Franklin Laureles <franklin.laureles.tindugan@ulb.ac.be>
2014-11-05 20:43:23 +0100

    Added JFileChooser for "Open Project" & "Save"

--- src/be/ac/ulb/infof307/g05/canvas/menu/FileMenu.java
+++ src/be/ac/ulb/infof307/g05/canvas/menu/FileMenu.java
@@ -1,14 +1,27 @@
 package be.ac.ulb.infof307.g05.canvas.menu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Vector;
 
+import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import be.ac.ulb.infof307.g05.EventControler;
 
 
-public class FileMenu extends AbstractMenu {
+public class FileMenu extends AbstractMenu implements ActionListener {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JMenuItem saveMenu = new JMenuItem("Save");
+	private JMenuItem openMenu = new JMenuItem("Open Project..");
+	private JMenuItem newMenu = new JMenuItem("New");
+	private JFileChooser fc = new JFileChooser();
 	
 	public FileMenu(EventControler eventControler){
 		this.setText("File");
@@ -18,8 +31,36 @@ public class FileMenu extends AbstractMenu {
 	}
 	
 	protected void createItems(){
-		_items.add(new JMenuItem("New"));
-		_items.add(new JMenuItem("Open File.."));
-		_items.add(new JMenuItem("Save"));
+		openMenu.addActionListener(this);
+		saveMenu.addActionListener(this);
+		_items.add(newMenu);
+		_items.add(openMenu);
+		_items.add(saveMenu);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object source = e.getSource();
+		
+		//Handle open button action
+		if (source == openMenu) {
+			int returnVal = fc.showOpenDialog(FileMenu.this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would open the file.
+            } else {
+                System.out.println("Error opening file.");
+            }
+		}
+		else if (source == saveMenu) {
+			int returnVal = fc.showSaveDialog(FileMenu.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would save the file.
+            } else {
+                
+            }
+		}
 	}
 }
