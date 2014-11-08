010b70af1d19c65268ea2529794f1b2daafaee1a
macornil <martin.cornil@ulb.ac.be>
2014-11-08 01:03:43 +0100

    [add] create reference (grid + coordinate)

---
+++ src/be/ac/ulb/infof307/g05/canvas/jme/Reference.java
@@ -0,0 +1,99 @@
+package be.ac.ulb.infof307.g05.canvas.jme;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.Node;
+import com.jme3.scene.VertexBuffer;
+import com.jme3.scene.debug.Grid;
+import com.jme3.util.BufferUtils;
+
+
+public class Reference {
+	private AssetManager _assetManager;
+	private Node		 _node;
+	private Geometry 	 _grid;
+	private Node    	 _coordinate = new Node("test");
+
+	
+	public Reference(AssetManager assetManager, int size){
+		_assetManager = assetManager;
+		createGrid(new Vector3f(), size, 1f, ColorRGBA.Gray);
+		createAxis(size, 1f);
+	}
+	
+	public void setNode(Node node, boolean attach){
+		if(_node != null){
+			setGridEnable(false);
+			setCoordEnable(false);
+		}
+		_node = node;
+		
+		if(attach){
+			setGridEnable(true);
+			setCoordEnable(true);
+		}
+	}
+	
+	public void setGridEnable(boolean enable){
+		if(enable)
+			_node.attachChild(_grid);
+		else
+			_node.detachChild(_grid);
+	}
+	
+	public void setCoordEnable(boolean enable){
+		if(enable)
+			_node.attachChild(_coordinate);
+		else
+			_node.detachChild(_coordinate);
+	}
+	
+    private void createGrid(Vector3f pos, int gridSize, float squareSize, ColorRGBA color){
+        _grid = new Geometry("grid", new Grid(gridSize, gridSize, squareSize) );
+        Material mat = new Material(_assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.getAdditionalRenderState().setWireframe(true);
+        mat.setColor("Color", color);
+        _grid.setMaterial(mat);
+        _grid.center().move(pos);
+      }
+	
+
+	public void createAxis(int lenght, float lineWidth){
+		//Axe X
+		Geometry x_axis = new Geometry("x axis", getMeshAxis((lenght/2), 0, 0, lineWidth));
+		setAxisColor(x_axis, ColorRGBA.Red);
+	    _coordinate.attachChild(x_axis);
+		//Axe Y
+		Geometry y_axis = new Geometry("y axis", getMeshAxis(0, (lenght/2), 0, lineWidth));
+		setAxisColor(y_axis, ColorRGBA.Green);
+		_coordinate.attachChild(y_axis);
+		//Axe Z
+		Geometry z_axis = new Geometry("z axis", getMeshAxis(0, 0, (lenght/2), lineWidth));
+		setAxisColor(z_axis, ColorRGBA.Blue);
+		_coordinate.attachChild(z_axis);
+	}
+	
+	private void setAxisColor(Geometry axis, ColorRGBA color){
+        Material mat = new Material(_assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", color);
+        axis.setMaterial(mat);
+	}
+	
+	public Mesh getMeshAxis(int coordX, int coordY, int coordZ, float lineWidth){
+		Mesh mesh = new Mesh();
+		Vector3f[] lineVerticies = new Vector3f[2];
+		lineVerticies[0]=new Vector3f(-coordX,-coordY,-coordZ);
+		lineVerticies[1]=new Vector3f( coordX, coordY, coordZ);
+
+        mesh.setMode(Mesh.Mode.Lines);
+        mesh.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(lineVerticies));
+        mesh.setLineWidth(lineWidth);
+
+        return mesh;
+    }
+	
+}
