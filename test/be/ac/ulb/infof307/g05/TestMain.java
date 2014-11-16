eea92d3c07f2d82ce48fb16ad8336ba35a1ce138
Antoine Carpentier <antoine.carpentier@ulb.ac.be>
2014-11-16 13:39:59 +0100

    [fix] tests ORM

--- test/be/ac/ulb/infof307/g05/TestMain.java
+++ test/be/ac/ulb/infof307/g05/TestMain.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 
 import java.sql.SQLException;
 
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+
 import be.ac.ulb.infof307.g05.model.*;
 
 public class TestMain {
@@ -29,8 +31,12 @@ public class TestMain {
 		Vertex vertex = new Vertex(0, 0, 0);
 		Mesh mesh = new Mesh("wall", "resources/wall.obj");
 		CompositeObject compositeObject1 = new CompositeObject(stage, vertex, vertex, mesh);
-		CompositeObject compositeObject2 = new CompositeObject(compositeObject1, vertex, vertex, mesh);
-		SimpleObject simpleObject1 = new SimpleObject(compositeObject2, vertex, vertex, mesh);
+			CompositeObject compositeObject2 = new CompositeObject(compositeObject1, vertex, vertex, mesh);
+				SimpleObject simpleObject1 = new SimpleObject(compositeObject2, vertex, vertex, mesh);
+				CompositeObject compositeObject5 = new CompositeObject(compositeObject2, vertex, vertex, mesh);
+			CompositeObject compositeObject3 = new CompositeObject(compositeObject1, vertex, vertex, mesh);
+			SimpleObject simpleObject2 = new SimpleObject(compositeObject1, vertex, vertex, mesh);
+		CompositeObject compositeObject4 = new CompositeObject(stage, vertex, vertex, mesh);
 
 		// save
 		project.create();
@@ -39,6 +45,10 @@ public class TestMain {
 		mesh.create();
 		compositeObject1.create();
 		compositeObject2.create();
+		compositeObject3.create();
+		compositeObject4.create();
+		compositeObject5.create();
 		simpleObject1.create();
+		simpleObject2.create();
 	}
 }
