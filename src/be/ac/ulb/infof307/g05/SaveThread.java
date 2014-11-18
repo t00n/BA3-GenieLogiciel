6605542f28b8efcb5dae2a3710d05f1a19d8256f
Antoine Carpentier <antoine.carpentier@ulb.ac.be>
2014-11-18 23:09:15 +0100

    [fix] automatic save every minute

--- src/be/ac/ulb/infof307/g05/SaveThread.java
+++ src/be/ac/ulb/infof307/g05/SaveThread.java
@@ -6,7 +6,7 @@ import be.ac.ulb.infof307.g05.model.Project;
 
 public class SaveThread extends Thread {
 	private Project _project;
-	private final long timeToSleep = 10000; // 1 minute
+	private final long timeToSleep = 60000; // 1 minute
 	
 	public SaveThread(Project project) {
 		this._project = project;
