92b2362baa41e504d1b5daa18b4e4d1b7a0d5075
Antoine Carpentier <antoine.carpentier@ulb.ac.be>
2014-11-19 16:28:56 +0100

    [fix] not working yet

--- src/be/ac/ulb/infof307/g05/EventController.java
+++ src/be/ac/ulb/infof307/g05/EventController.java
@@ -4,8 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 import java.util.Vector;
+
+import javax.swing.JComboBox;
 
 import be.ac.ulb.infof307.g05.MainWindow;
 import be.ac.ulb.infof307.g05.SaveThread;
@@ -31,6 +35,56 @@ public class EventController implements ActionListener {
 	private Project		 _currentProject;
 	private Dao<Project, Integer> _daoProject = Project.getDao(Project.class);
 	private List<Project> _projects;
+	
+
+	private HashMap<String, Boolean> _flagTools = new HashMap<String, Boolean>();
+	private Stack<Vector3f> 		 _positionStack = new Stack<Vector3f>();
+	private String					 _lastCollision;
+	private Vector3f				 _cursor = new Vector3f();
+	
+	public void addTool(String tool_name){
+		/** this method add a flag for the new tool **/
+		_flagTools.put(tool_name, false);
+	}
+	
+	public void addPosition(){
+		/** this method push a new vector to position stack if a tool is active **/
+		if(!getEnabledTool().isEmpty()){
+			_positionStack.push(new Vector3f(_cursor));
+			System.out.println("[DEBUG][ToolController::addPosition] : " + _positionStack);
+		}
+	}
+	
+	public void enableTool(String tool_name){
+		/** this method enable a tool and disable others **/
+		if(_flagTools.containsKey(tool_name)){
+			System.out.println("[DEBUG][ToolController::enableTool] : " + tool_name);
+			for(String tool:_flagTools.keySet()){
+				if(tool.equals(tool_name))
+					_flagTools.put(tool_name, !_flagTools.get(tool_name));
+				else
+				_flagTools.put(tool, false);
+			}
+			purge();
+		}
+	}
+	
+	public String getEnabledTool(){
+		/** this method return the active tool name **/
+		String enabled_tool = new String();
+		
+		for(String tool:_flagTools.keySet()){
+			if(_flagTools.get(tool)){
+				enabled_tool += tool;
+			}
+		}
+		return enabled_tool;
+	}
+
+	private void purge(){
+		/** this method pop all position from position stack (except cursor) **/
+		_positionStack.removeAllElements();
+	}
 	
 	private SaveThread _saveThread;
 	
@@ -58,14 +112,9 @@ public class EventController implements ActionListener {
 		return _toolController.getCursor();
 	}
 	
-	public void addTool(String tool_name){
-		/** this method add a flag for the new tool **/
-		_toolController.addTool(tool_name);
-	}
-	
 	public CompositeObject getStage(){
 		/** this method return the root node of the scene built by toolController **/
-		return _toolController.getStage();
+		return _currentProject.getStage(0).getFloor();
 	}
 	
 	public String getEnableTool(){
@@ -178,6 +227,50 @@ public class EventController implements ActionListener {
 		/** this method manage events **/
 		String command = event.getActionCommand();
 		
+		if(command == "cursor_move"){
+			// cursor is moving
+			_cursor.set((Vector3f) event.getSource());
+		}else if(getEnabledTool().isEmpty() && command != "comboBoxChanged"){
+			// user tries to enable a tool (!= comboBoxChanged because need to get the default value of comboBoxes from options, event send before)
+			enableTool(command);
+		}else{
+			// when a tool is enabled
+			if(command == "ENTER"){
+				//FIXME if(drawing && current_drawing_object.getType() == polygon) force draw, attach to father, current_drawing_object = new object(type=polygon)
+			}else if(command == "ESCAPE"){
+				purge();
+			}else if(command == "collision"){
+				_lastCollision = (String) event.getSource();
+				System.out.println("[DEBUG][ToolController::ActionPerformed] Collision : " + _lastCollision);
+				//FIXME if(drawing && current_drawing_object.getFather() == null) current_drawing_object.setFather(_lastCollision)
+			}else if(command == "cursor_click_up"){
+				//FIXME if(drawing && current_drawing_object.addPosition(_cursor) == true) attach to father, current_drawing_object = new object(type=last drawn)
+				addPosition();
+				if (_positionStack.size() == 2) {
+					System.out.println("[Debug][ToolController::actionPerformed] : Draw");
+					Vector3f one = _positionStack.pop();
+					Vector3f two = _positionStack.pop();
+					two.y += 0.1;
+					Cube newMesh = new Cube(one, two);
+					_currentProject.getStage(0).getFloor().add(new CompositeObject(_currentProject.getStage(0).getFloor(), newMesh.getVertices(), newMesh.getOrder()));
+				}
+			}else if(command == "comboBoxChanged"){
+				String option_choice = ((JComboBox)(event.getSource())).getSelectedItem().toString();
+				
+				if(getEnabledTool().equals("Draw")){
+					purge();
+					if(option_choice == "Rectangle"){
+						//FIXME current_drawing_object = new rectangle
+					}else if(option_choice == "Polygon"){
+						//FIXME current_drawing_object = new polygon
+					}else if(option_choice == "Oval"){
+						//FIXME current_drawing_object = new oval
+					}
+				}
+			}else{
+				enableTool(command);
+			}
+		}
 		if(command == "2D view"){
 			_flag2D = true;
 			_flag3D = false;
