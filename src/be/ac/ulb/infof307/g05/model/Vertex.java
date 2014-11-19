6bf5cc89fa8917a95f07a824780d29cae9e304d6
macornil <martin.cornil@ulb.ac.be>
2014-11-19 17:06:27 +0100

    [refactor] minor

--- src/be/ac/ulb/infof307/g05/model/Vertex.java
+++ src/be/ac/ulb/infof307/g05/model/Vertex.java
@@ -8,6 +8,18 @@ import com.jme3.math.Vector3f;
 
 @DatabaseTable (tableName = "vertexes")
 public class Vertex extends Database<Vertex> {
+	
+	@DatabaseField (generatedId = true)
+	protected int id_vertex;
+	@DatabaseField (canBeNull = false)
+	protected float x;
+	@DatabaseField (canBeNull = false)
+	protected float y;
+	@DatabaseField (canBeNull = false)
+	protected float z;
+	@DatabaseField (canBeNull = false, foreign = true)
+	private CompositeObject referent;
+	
 	protected Vertex() {
 		
 	}
@@ -20,15 +32,14 @@ public class Vertex extends Database<Vertex> {
 	
 	public Vertex(CompositeObject referent, Vector3f vec) {
 		this.referent = referent;
-		this.x = vec.x;
-		this.y = vec.y;
-		this.z = vec.z;
+		this.x = vec.getX();
+		this.y = vec.getY();
+		this.z = vec.getZ();
 	}
 	
-	public Vector3f getVector() { return this.toVector3f(); }
-	
-	@DatabaseField (generatedId = true)
-	protected int id_vertex;
+	public Vector3f getVector() {
+		return this.toVector3f();
+	}
 
     public void save() {
         try {
@@ -43,26 +54,18 @@ public class Vertex extends Database<Vertex> {
 		return this.id_vertex;
 	}
 
-	@DatabaseField (canBeNull = false)
-	protected float x;
+
 	public void setX(Float x) {
 		this.x = x;
 	}
 	
-	@DatabaseField (canBeNull = false)
-	protected float y;
 	public void setY(Float y) {
 		this.y = y;
 	}
 	
-	@DatabaseField (canBeNull = false)
-	protected float z;
 	public void setZ(Float z) {
 		this.z = z;
 	}
-	
-	@DatabaseField (canBeNull = false, foreign = true)
-	private CompositeObject referent;
 
 	public Vector3f toVector3f() {
 		return new Vector3f(this.x, this.y, this.z);
