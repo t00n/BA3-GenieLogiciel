54cc138e9487d5bfab7e97a6db7347b7c929da6e
Antoine Carpentier <antoine.carpentier@ulb.ac.be>
2014-11-24 21:22:10 +0100

    [fix] new Composite Object

--- src/be/ac/ulb/infof307/g05/model/CompositeObject.java
+++ src/be/ac/ulb/infof307/g05/model/CompositeObject.java
@@ -1,14 +1,10 @@
 package be.ac.ulb.infof307.g05.model;
 
-import be.ac.ulb.infof307.g05.view.Cube;
-
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-import com.jme3.math.Vector3f;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.lang.Math;
@@ -20,97 +16,66 @@ import java.lang.Math;
 public class CompositeObject extends Database<CompositeObject> implements Iterable<CompositeObject> {	
 	
 	@DatabaseField (id=true)
-	private Integer id_compositeObject;
+	protected Integer id_compositeObject;
+	
 	@DatabaseField (canBeNull = true, foreign = true)
-	private CompositeObject parent;
-	@DatabaseField (canBeNull = true, foreign = true, foreignAutoRefresh = true)
-	private Texture texture;
+	protected CompositeObject parent;
+	
+	@DatabaseField (canBeNull = true, foreign = true)
+	protected Room room;
+	
+	@DatabaseField (canBeNull = true, foreign = true)
+	protected Resource texture;
+	
+	@DatabaseField (canBeNull = false, foreign = true)
+	protected Resource mesh;
 
-	protected Collection<Vertex> vertices;
-	protected Collection<Integer> meshOrder;
 	protected Collection<CompositeObject> childs;
 	
 	protected CompositeObject() {
 		
 	}
     
-	public CompositeObject(CompositeObject parent, Collection<Vector3f> vertices, Collection<Integer> meshOrder) {
+	public CompositeObject(CompositeObject parent, Resource mesh) {
+		this.init(parent, null, mesh, null);
+	}
+	
+	public CompositeObject(CompositeObject parent, Resource mesh, Resource texture) {
+		this.init(parent, null, mesh, texture);
+	}
+	
+	public CompositeObject(Room room, Resource mesh) {
+		this.init(null, room, mesh, null);
+	}
+	
+	public CompositeObject(Room room, Resource mesh, Resource texture) {
+		this.init(null, room, mesh, texture);
+	}
+	
+	private void init(CompositeObject parent, Room room, Resource mesh, Resource texture) {
 		this.parent = parent;
-		this.vertices = toVertex(vertices);
-		this.meshOrder = meshOrder;
-		this.texture = null;
+		this.room = room;
+		this.mesh = mesh;
+		this.texture = texture;
 		this.isNew = true;
 	}
 	
-	private Collection<Vertex> toVertex(Collection<Vector3f> vertices) {
-		Collection<Vertex> ret = new ArrayList<Vertex>();
-		for (Vector3f vec: vertices) {
-			Vertex vertex = new Vertex(this, vec);
-			ret.add(vertex);
-		}
-		return ret;
-	}
-
-	public Integer getId() {
+	public int getId() {
 		return this.id_compositeObject;
 	}
 	
 	public void setId() {
-		CompositeObject_ID id = new CompositeObject_ID();
-		try {
-			id.create();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		this.id_compositeObject = id.getId();
+		this.id_compositeObject = Static_ID.getCompositeObjectID();
 	}
 
-	public Texture getTexture() {
+	public Resource getTexture() {
 		return this.texture;
 	}
 	
-	public Vector3f[] getVerticesAsVector3f() {
-		Vector3f[] ret = new Vector3f[this.getVertices().size()];
-		int i = 0;
-		for (Vertex vertex: this.getVertices()) {
-			ret[i] = vertex.toVector3f();
-			++i;
-		}
-		return ret;
+	public Resource getMesh() {
+		return this.mesh;
 	}
 	
-	public int[] getMeshOrderAsInt() {
-		int[] ret = new int[this.getMeshOrder().size()];
-		int i = 0;
-		for (Integer order: this.getMeshOrder()) {
-			ret[i] = order;
-			++i;
-		}
-		return ret;
-	}
-	
-	// Database
-	public Collection<Vertex> getVertices() {
-		if (this.vertices == null) {
-			Dao<Vertex, Integer> dao = Vertex.getDao(Vertex.class);
-			try {
-				this.vertices = dao.queryForEq("referent_id", this.getId());
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return this.vertices;
-	}
-	
-	public Collection<Integer> getMeshOrder() {
-		ArrayList<Vertex> vectors = (ArrayList<Vertex>) this.getVertices();
-		Cube cube = new Cube(vectors.get(0), vectors.get(7));
-		this.meshOrder = cube.getOrder();
-		return this.meshOrder;
-	}
-
 	public Collection<CompositeObject> getChilds() {
 		if (this.childs == null) {
 			Dao<CompositeObject, Integer> dao = CompositeObject.getDao(CompositeObject.class);
@@ -138,77 +103,75 @@ public class CompositeObject extends Database<CompositeObject> implements Iterab
 		return ret;
 	}
 	
-	public void extendAxeY(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		for (int i = (tmp.size()/2); i < (tmp.size()); ++i) {
-			tmp.get(i).y += range;
-		}
-	}
-	
-	public void moveAxeX(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		for (int i = 0; i < (tmp.size()); ++i) {
-			tmp.get(i).x += range;
-		}
-	}
-	
-	public void moveAxeZ(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		for (int i = 0; i < (tmp.size()); ++i) {
-			tmp.get(i).z += range;
-		}
-	}
-	
-	public void moveAxeY(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		for (int i = 0; i < (tmp.size()); ++i) {
-			tmp.get(i).y += range;
-		}
-	}
-	
-	public void rotateAroundZ(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		float posX;
-		float posY;
-		for (int i = 0; i < (tmp.size()); ++i) {
-			posX = tmp.get(i).x;
-			posY = tmp.get(i).y;
-			tmp.get(i).x = (float) ((posX*Math.cos(range)) - (posY*Math.sin(range)));
-			tmp.get(i).y = (float) ((posY*Math.cos(range)) + (posX*Math.sin(range)));
-		}
-	}
-
-	public void rotateAroundY(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		float posX;
-		float posZ;
-		for (int i = 0; i < (tmp.size()); ++i) {
-			posX = tmp.get(i).x;
-			posZ = tmp.get(i).z;
-			tmp.get(i).x = (float) ((posX*Math.cos(range)) - (posZ*Math.sin(range)));
-			tmp.get(i).z = (float) ((posZ*Math.cos(range)) + (posX*Math.sin(range)));
-		}
-	}
-	
-	public void rotateAroundX(float range) {
-		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
-		float posY;
-		float posZ;
-		for (int i = 0; i < (tmp.size()); ++i) {
-			posY = tmp.get(i).y;
-			posZ = tmp.get(i).z;
-			tmp.get(i).y = (float) ((posY*Math.cos(range)) - (posZ*Math.sin(range)));
-			tmp.get(i).z = (float) ((posZ*Math.cos(range)) + (posY*Math.sin(range)));
-		}
-	}
+//	public void extendAxeY(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		for (int i = (tmp.size()/2); i < (tmp.size()); ++i) {
+//			tmp.get(i).y += range;
+//		}
+//	}
+//	
+//	public void moveAxeX(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		for (int i = 0; i < (tmp.size()); ++i) {
+//			tmp.get(i).x += range;
+//		}
+//	}
+//	
+//	public void moveAxeZ(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		for (int i = 0; i < (tmp.size()); ++i) {
+//			tmp.get(i).z += range;
+//		}
+//	}
+//	
+//	public void moveAxeY(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		for (int i = 0; i < (tmp.size()); ++i) {
+//			tmp.get(i).y += range;
+//		}
+//	}
+//	
+//	public void rotateAroundZ(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		float posX;
+//		float posY;
+//		for (int i = 0; i < (tmp.size()); ++i) {
+//			posX = tmp.get(i).x;
+//			posY = tmp.get(i).y;
+//			tmp.get(i).x = (float) ((posX*Math.cos(range)) - (posY*Math.sin(range)));
+//			tmp.get(i).y = (float) ((posY*Math.cos(range)) + (posX*Math.sin(range)));
+//		}
+//	}
+//
+//	public void rotateAroundY(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		float posX;
+//		float posZ;
+//		for (int i = 0; i < (tmp.size()); ++i) {
+//			posX = tmp.get(i).x;
+//			posZ = tmp.get(i).z;
+//			tmp.get(i).x = (float) ((posX*Math.cos(range)) - (posZ*Math.sin(range)));
+//			tmp.get(i).z = (float) ((posZ*Math.cos(range)) + (posX*Math.sin(range)));
+//		}
+//	}
+//	
+//	public void rotateAroundX(float range) {
+//		ArrayList<Vertex> tmp = (ArrayList<Vertex>) this.getVertices();
+//		float posY;
+//		float posZ;
+//		for (int i = 0; i < (tmp.size()); ++i) {
+//			posY = tmp.get(i).y;
+//			posZ = tmp.get(i).z;
+//			tmp.get(i).y = (float) ((posY*Math.cos(range)) - (posZ*Math.sin(range)));
+//			tmp.get(i).z = (float) ((posZ*Math.cos(range)) + (posY*Math.sin(range)));
+//		}
+//	}
 	
     @Override
     public void save() {
         if (this.texture != null)
             this.texture.save();
         super.save();
-        for (Vertex position : this.getVertices())
-            position.save();
         for (CompositeObject object : this.getChilds())
         	object.save();
     }
@@ -226,7 +189,7 @@ public class CompositeObject extends Database<CompositeObject> implements Iterab
 	public void addChild(CompositeObject parent, CompositeObject child) {
 		if (parent == this) {
 			this.add(child);
-			child.setId();
+//			child.setId(); FIXME
 		}
 		else
 		{
